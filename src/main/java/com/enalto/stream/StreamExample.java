1898194b1740f3b55c0e188ae1afafafc4dc6628
enalto <enaltogondrige@gmail.com>
2024-11-20 20:34:15 -0300

    Refactoring class StreamExample.

--- src/main/java/com/enalto/stream/StreamExample.java
+++ src/main/java/com/enalto/stream/StreamExample.java
@@ -1,9 +1,6 @@
 package com.enalto.stream;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,12 +11,22 @@ public class StreamExample {
         List<Book> list = new ArrayList<>();
 
         list.add(new Book("Book 1", 500, BookType.HISTORY));
-        list.add(new Book("Book 2", 250, BookType.PHILOSOPHY));
-        list.add(new Book("Book 3", 150, BookType.NOVEL));
-        list.add(new Book("Book 4", 550, BookType.HISTORY));
+        list.add(new Book("Book 2 dhjdhsd", 250, BookType.PHILOSOPHY));
+        list.add(new Book("Book 3 ghghgh", 150, BookType.NOVEL));
+        list.add(new Book("Book 4 mmmm", 550, BookType.HISTORY));
         list.add(new Book("Book 5", 350, BookType.PHILOSOPHY));
         list.add(new Book("Book 6", 241, BookType.THRILLER));
-        list.add(new Book("Book 7", 220, BookType.HISTORY));
+        list.add(new Book("Book 7 vvvvv", 220, BookType.HISTORY));
+
+
+        List<Book> list1 = list.stream().filter(book -> {
+            String[] worlds = book.getTitle().split(" ");
+            return worlds.length == 2;
+        }).collect(Collectors.toList());
+
+        System.out.println(list1);
+
+        System.out.println("-".repeat(100));
 
         List<Book> bookList = list.stream().filter(book -> book.getBookType() == BookType.PHILOSOPHY)
                 .sorted(Comparator.comparing(book -> book.getTitle()))
@@ -34,9 +41,29 @@ public class StreamExample {
         Map<BookType, List<Book>> map = list.stream()
                 .collect(Collectors.groupingBy(Book::getBookType));
 
-        for(Map.Entry entry: map.entrySet()){
+        for (Map.Entry entry : map.entrySet()) {
             System.out.println(entry);
         }
 
+        System.out.println("-".repeat(100));
+
+        System.out.println("longest book >"+getLongestBook(list));
+    }
+
+    public List<List<Integer>> generatePairs() {
+
+        List<Integer> nums1 = Arrays.asList(1, 2, 3);
+        List<Integer> nums2 = Arrays.asList(4, 5);
+
+        List<List<Integer>> pairs = nums1.stream()
+                .flatMap(i -> nums2.stream().map(j -> Arrays.asList(i, j))).collect(Collectors.toList());
+
+        return pairs;
+    }
+
+    public static Optional<Book> getLongestBook(List<Book> list) {
+        Optional<Book> bookOptional = list.stream()
+                .reduce((book1, book2) -> book1.getPages() > book2.getPages() ? book1 : book2);
+        return bookOptional;
     }
 }
